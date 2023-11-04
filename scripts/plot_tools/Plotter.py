d136b5f059b2108cb79394ec3aa1d6fa0170ccb1
rthete <reikattt@qq.com>
2023-11-04 17:57:41 +0800

    refactor: plot scripts

---
+++ scripts/plot_tools/Plotter.py
@@ -0,0 +1,153 @@
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+from mpl_toolkits.axisartist.axislines import Subplot
+import numpy as np
+
+
+class Plotter():
+
+    def plot_error_time_bar(x, x_data, APMISRR_avg, APMISRR_diff, TolerMIS_avg, TolerMIS_diff, SIS_avg, SIS_diff, name="name"):
+        """
+        容错-时间-柱状图
+        """
+        config = {
+            "font.family": 'serif',  # 衬线字体
+            "font.size": 13,  # 相当于小四大小
+            "font.serif": ['SimSun'],  # 宋体
+            "mathtext.fontset": 'stix',  # matplotlib渲染数学字体时使用的字体，和Times New Roman差别不大
+            'axes.unicode_minus': False  # 处理负号，即-号
+        }
+        rcParams.update(config)
+
+        fig = plt.figure(figsize=(5.8, 3.7), dpi=2000)
+        ax = Subplot(fig, 111)
+        fig.add_subplot(ax)
+
+        ax.axis["bottom"].set_axisline_style("->", size=1)
+        ax.axis["left"].set_axisline_style("->", size=1)
+        ax.axis["top"].set_visible(False)
+        ax.axis["right"].set_visible(False)
+
+        x = np.array(x)
+        total_width, n = 0.9, 3
+        width = total_width / n
+        x = x - (total_width - width) / 2
+
+        err_attr = {"elinewidth": 0.8, "ecolor": "black", "capsize": 2.5}
+        plt.bar(x, SIS_avg, yerr=SIS_diff, width=width,
+                label=r'$\mathrm{SIS}$', color='#A5A5A5', error_kw=err_attr)
+        plt.bar(x + width, APMISRR_avg, yerr=APMISRR_diff, width=width,
+                label=r'$\mathrm{APMISRR}$', color='#5B9BD5', error_kw=err_attr)
+        plt.bar(x + 2 * width, TolerMIS_avg, yerr=TolerMIS_diff, width=width,
+                label=r'$\mathrm{TolerMIS}$', color='#ED7D31', error_kw=err_attr)
+
+        plt.legend(prop={"family": "Times New Roman", 'size': 9},
+                   bbox_to_anchor=(0.28, 0.98))  # 显示上面的label
+        plt.yticks(fontproperties='Times New Roman', size=6)
+        plt.xticks(fontproperties='Times New Roman', size=6)
+
+        plt.xlabel(r'$\mathit{W(1e3)}$', fontsize=9)
+        plt.ylabel(r'$\mathrm{Time}$', fontsize=9)
+        # plt.yscale("log")
+        plt.xticks(x_data)
+
+        # plt.ticklabel_format(style='scientific', scilimits=(0, 2), axis='y')
+        # plt.show(block = True)
+
+        plt.savefig(name, dpi=1000)
+
+    def plot_error_ur(x_data, name="name"):
+        """
+        容错-利用率-箱线图
+        """
+        config = {
+            "font.family": 'serif',  # 衬线字体
+            "font.size": 12.5,  # 相当于小四大小
+            "font.serif": ['SimSun'],  # 宋体
+            "mathtext.fontset": 'stix',  # matplotlib渲染数学字体时使用的字体，和Times New Roman差别不大
+            'axes.unicode_minus': False  # 处理负号，即-号
+        }
+        rcParams.update(config)
+
+        fig = plt.figure(figsize=(5, 8), dpi=2000)
+
+        ax = Subplot(fig, 111)
+        fig.add_subplot(ax)
+
+        ax.axis["bottom"].set_axisline_style("->", size=1)
+        ax.axis["left"].set_axisline_style("->", size=1)
+        ax.axis["top"].set_visible(False)
+        ax.axis["right"].set_visible(False)
+
+        # ax.set_ylim(70, 100)
+        ax.set_yticks(np.linspace(70, 100, 11, endpoint=True))
+
+        box = plt.boxplot(x_data,
+                          labels=[
+                              '$\mathrm{SIS}$', '$\mathrm{APMISRR}$', '$\mathrm{TolerMIS}$'],
+                          vert=True,
+                          widths=0.4,
+                          meanline=False,
+                          showmeans=False,
+                          patch_artist=True,
+                          medianprops={'color': 'black'})
+
+        plt.ylabel(r'$\mathrm{using \: rate(\%)}$', fontsize=12)
+
+        c_list = ['#A5A5A5', '#5B9BD5', '#ED7D31']
+
+        for a, c in zip(box['boxes'], c_list):
+            a.set(color='black')
+            a.set(facecolor=c)
+
+        plt.savefig(name, dpi=1000)
+        
+    def plot_error_time_box(x, x_data, APMISRR_avg, APMISRR_diff, TolerMIS_avg, TolerMIS_diff, SIS_avg, SIS_diff, name="name"):
+        """
+        容错-时间-箱线图
+        """
+        config = {
+            "font.family": 'serif',  # 衬线字体
+            "font.size": 13,  # 相当于小四大小
+            "font.serif": ['SimSun'],  # 宋体
+            "mathtext.fontset": 'stix',  # matplotlib渲染数学字体时使用的字体，和Times New Roman差别不大
+            'axes.unicode_minus': False  # 处理负号，即-号
+        }
+        rcParams.update(config)
+
+        fig = plt.figure(figsize=(5.8, 3.7), dpi=2000)
+        ax = Subplot(fig, 111)
+        fig.add_subplot(ax)
+
+        ax.axis["bottom"].set_axisline_style("->", size=1)
+        ax.axis["left"].set_axisline_style("->", size=1)
+        ax.axis["top"].set_visible(False)
+        ax.axis["right"].set_visible(False)
+
+        x = np.array(x)
+        total_width, n = 0.9, 3
+        width = total_width / n
+        x = x - (total_width - width) / 2
+
+        err_attr = {"elinewidth": 0.8, "ecolor": "black", "capsize": 2.5}
+        plt.bar(x, SIS_avg, yerr=SIS_diff, width=width,
+                label=r'$\mathrm{SIS}$', color='#A5A5A5', error_kw=err_attr)
+        plt.bar(x + width, APMISRR_avg, yerr=APMISRR_diff, width=width,
+                label=r'$\mathrm{APMISRR}$', color='#5B9BD5', error_kw=err_attr)
+        plt.bar(x + 2 * width, TolerMIS_avg, yerr=TolerMIS_diff, width=width,
+                label=r'$\mathrm{TolerMIS}$', color='#ED7D31', error_kw=err_attr)
+
+        plt.legend(prop={"family": "Times New Roman", 'size': 9},
+                   bbox_to_anchor=(0.28, 0.98))  # 显示上面的label
+        plt.yticks(fontproperties='Times New Roman', size=6)
+        plt.xticks(fontproperties='Times New Roman', size=6)
+
+        plt.xlabel(r'$\mathit{W(1e3)}$', fontsize=9)
+        plt.ylabel(r'$\mathrm{Time}$', fontsize=9)
+        # plt.yscale("log")
+        plt.xticks(x_data)
+
+        # plt.ticklabel_format(style='scientific', scilimits=(0, 2), axis='y')
+        # plt.show(block = True)
+
+        plt.savefig(name, dpi=1000)
---
+++ scripts/plot_tools/utils.py
@@ -0,0 +1,87 @@
+"""
+FilePath: /InstallmentScheduling/scripts/plot_tools/utils.py
+Description:  
+Author: rthete
+Date: 2023-11-04 16:19:15
+LastEditTime: 2023-11-04 16:22:33
+"""
+import numpy as np
+np.random.seed(1)
+
+
+def read_data_from_file(file_path):
+    """
+    读取数据并存入数组
+    """
+    with open(file_path, 'r') as file:
+        # 初始化一个空二维数组来存储读取的数据
+        data = []
+
+        # 逐行读取文件内容
+        for line in file:
+            # 使用strip()方法去除每行末尾的换行符和空白字符
+            line = line.strip()
+
+            # 使用逗号分隔符将每行的内容拆分为多个数
+            numbers_str = line.split(',')
+
+            # 将每个字符串数值转换为整数，并添加到二维数组中
+            row = []
+            for num_str in numbers_str:
+                try:
+                    number = float(num_str)
+                    row.append(number)
+                except ValueError:
+                    print(f"无法将 '{num_str}' 转换为整数。")
+
+            data.append(row)
+
+    # 返回二维数组
+    return data
+
+
+def generate_data_list(error_num):
+    """
+    从数据生成数组，返回error_num时的三个模型mock数据，格式为
+    [
+        [SIS数据]
+        [APMISRR数据]
+        [TolerMIS数据]
+    ]
+    """
+
+    SIS_max = read_data_from_file('./output/exp_3_ur/error_SIS_30_max.csv')
+    SIS_min = read_data_from_file('./output/exp_3_ur/error_SIS_30_min.csv')
+
+    APMISRR_max = read_data_from_file(
+        './output/exp_3_ur/error_APMISRR_30_max.csv')
+    APMISRR_min = read_data_from_file(
+        './output/exp_3_ur/error_APMISRR_30_min.csv')
+
+    TolerMIS_max = read_data_from_file(
+        './output/exp_3_ur/error_TolerMIS_30_max.csv')
+    TolerMIS_min = read_data_from_file(
+        './output/exp_3_ur/error_TolerMIS_30_min.csv')
+
+    list_SIS = np.array([SIS_max[error_num - 1][0] * 100])
+    list_SIS = np.append(list_SIS, np.random.uniform(SIS_min[error_num - 1][0] * 100,
+                                                     SIS_max[error_num - 1][0] * 100, 88))
+    list_SIS = np.append(list_SIS, SIS_min[error_num - 1][0] * 100)
+
+    list_APMISRR = np.array([APMISRR_max[error_num - 1][0] * 100])
+    list_APMISRR = np.append(list_APMISRR, np.random.uniform(APMISRR_min[error_num - 1][0] * 100,
+                                                             APMISRR_max[error_num - 1][0] * 100, 88))
+    list_APMISRR = np.append(list_APMISRR, APMISRR_min[error_num - 1][0] * 100)
+
+    list_TolerMIS = np.array([TolerMIS_max[error_num - 1][0] * 100])
+    list_TolerMIS = np.append(list_TolerMIS, np.random.uniform(TolerMIS_min[error_num - 1][0] * 100,
+                                                               TolerMIS_max[error_num - 1][0] * 100, 88))
+    list_TolerMIS = np.append(
+        list_TolerMIS, TolerMIS_min[error_num - 1][0] * 100)
+
+    list_data = []
+    list_data.append(list_SIS)
+    list_data.append(list_APMISRR)
+    list_data.append(list_TolerMIS)
+
+    return list_data
