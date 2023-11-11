79850d8e570b58e9741cc764026ab1827c9461b8
rthete <reikattt@qq.com>
2023-11-11 18:58:43 +0800

    style: format exp_3.cpp

--- exp/exp_3.cpp
+++ exp/exp_3.cpp
@@ -3,7 +3,7 @@
  * @Description: 带容错的3个模型实验
  * @Author: rthete
  * @Date: 2023-08-19 17:41:41
- * @LastEditTime: 2023-11-04 15:47:06
+ * @LastEditTime: 2023-11-04 21:50:40
  */
 #include "exp_3.h"
 
@@ -97,14 +97,18 @@ void error_TolerMIS_30_conflict() {
   /* 任务量5000/10000 */
   for (const auto &row : error_places) {
     vector<double> result;
-    ModelRunner::run_MISRR_conflict(result, 30, 0, 5000, 0.3, "../data/exp1-30-servers/",
-                       row, error_installment[0][0]);
-    // ModelRunner::run_MISRR_conflict(result, 30, 0, 10000, 0.3, "../data/exp1-30-servers/",
+    ModelRunner::run_MISRR_conflict(result, 30, 0, 5000, 0.3,
+                                    "../data/exp1-30-servers/", row,
+                                    error_installment[0][0]);
+    // ModelRunner::run_MISRR_conflict(result, 30, 0, 10000, 0.3,
+    // "../data/exp1-30-servers/",
     //                    row, error_installment[1][0]);
     // 对比没有冲突情况
-    ModelRunner::run_MISRR_conflict(result, 30, 0, 5000, 0.3, "../data/exp1-30-servers/",
-                       row, error_installment[2][0]);
-    // ModelRunner::run_MISRR_conflict(result, 30, 0, 10000, 0.3, "../data/exp1-30-servers/",
+    ModelRunner::run_MISRR_conflict(result, 30, 0, 5000, 0.3,
+                                    "../data/exp1-30-servers/", row,
+                                    error_installment[2][0]);
+    // ModelRunner::run_MISRR_conflict(result, 30, 0, 10000, 0.3,
+    // "../data/exp1-30-servers/",
     //                    row, error_installment[3][0]);
     for (auto it = result.begin(); it != result.end(); ++it) {
       conflictFile << *it;
@@ -140,7 +144,8 @@ void error_SIS_30() {
 
       /* 每种30次取平均 */
       for (const auto &row : error_places) {
-        double result = ModelRunner::run_SIS(30, w, 0.3, "../data/exp1-30-servers/", row);
+        double result =
+            ModelRunner::run_SIS(30, w, 0.3, "../data/exp1-30-servers/", row);
         result_sum += result;
         result_min = std::min(result_min, result);
         result_max = std::max(result_max, result);
@@ -192,8 +197,9 @@ void error_TolerMIS_30() {
 
       /* 每种30次取平均 */
       for (const auto &row : error_places) {
-        double result = ModelRunner::run_MISRR(30, 0, w, 0.3, "../data/exp1-30-servers/",
-                                  row, error_installment[index_w][index_30++]);
+        double result =
+            ModelRunner::run_MISRR(30, 0, w, 0.3, "../data/exp1-30-servers/",
+                                   row, error_installment[index_w][index_30++]);
         result_sum += result;
         result_min = std::min(result_min, result);
         result_max = std::max(result_max, result);
@@ -248,9 +254,9 @@ void error_APMISRR_30() {
       double result_min = 100000, result_max = 0;
       /* 每种30次取平均 */
       for (const auto &row : error_places) {
-        double result = ModelRunner::run_myAPMISRR(30, 0.5, m[index_w], w, 0.3,
-                                      "../data/exp1-30-servers/", row,
-                                      error_installment[index_w][index_30++]);
+        double result = ModelRunner::run_myAPMISRR(
+            30, 0.5, m[index_w], w, 0.3, "../data/exp1-30-servers/", row,
+            error_installment[index_w][index_30++]);
         result_sum += result;
         result_min = std::min(result_min, result);
         result_max = std::max(result_max, result);
@@ -271,10 +277,10 @@ void error_APMISRR_30() {
   diffFile.close();
   maxFile.close();
   minFile.close();
-//   transposeCSV("../output/exp_3/error_APMISRR_30_mean.csv",
-//                "../output/exp_3/t_error_APMISRR_30_mean.csv");
-//   transposeCSV("../output/exp_3/error_APMISRR_30_diff.csv",
-//                "../output/exp_3/t_error_APMISRR_30_diff.csv");
+  //   transposeCSV("../output/exp_3/error_APMISRR_30_mean.csv",
+  //                "../output/exp_3/t_error_APMISRR_30_mean.csv");
+  //   transposeCSV("../output/exp_3/error_APMISRR_30_diff.csv",
+  //                "../output/exp_3/t_error_APMISRR_30_diff.csv");
 }
 
 /*利用率，3个模型，共30个处理机，故障1/2/3/4个处理机，任务量5000，每种实验30次
@@ -282,9 +288,12 @@ void error_APMISRR_30() {
 void error_cmp_3_models_ur() {
   std::vector<std::vector<int>> error_places;
   std::vector<std::vector<int>> error_installment;
+  vector<int> m = {19, 21, 23, 25, 26, 28, 29, 30, 31, 33, 34};
+
+  std::ofstream allFile("../output/exp_3_all_exps/error_ur_s30_w5000.csv");
+  allFile << "model,error_num,value" << std::endl;
 
   /* 故障i个处理机 */
-  vector<int> m = {19, 21, 23, 25, 26, 28, 29, 30, 31, 33, 34};
   for (int i = 1; i <= 4; ++i) {
     error_places.clear();
     readTXTFile(
@@ -292,8 +301,8 @@ void error_cmp_3_models_ur() {
         error_places);
     readTXTFile("../data/exp3-error-place/error-installment-2.txt",
                 error_installment);
-    std::ofstream singleFile("../output/exp_3_all_exps/error_ur_30_" +
-                             std::to_string(i) + ".csv");
+    // std::ofstream singleFile("../output/exp_3_all_exps/error_ur_30_" +
+    //                          std::to_string(i) + ".csv");
     /* 任务量w */
     int index_w = 0;
     for (int w = 5000; w < 5500; w += 1000) {
@@ -301,20 +310,28 @@ void error_cmp_3_models_ur() {
 
       /* 每种30次 */
       for (const auto &row : error_places) {
-        double result = ModelRunner::run_SIS(30, w, 0.3, "../data/exp1-30-servers/", row);
-        singleFile << result << ",";
-        result = ModelRunner::run_MISRR(30, 0, w, 0.3, "../data/exp1-30-servers/", row,
-                           error_installment[index_w][index_30]);
-        singleFile << result << ",";
-        result = ModelRunner::run_myAPMISRR(30, 0.5, m[index_w], w, 0.3,
-                               "../data/exp1-30-servers/", row,
-                               error_installment[index_w][index_30]);
-        singleFile << result << "\n";
+        double result =
+            ModelRunner::run_SIS(30, w, 0.3, "../data/exp1-30-servers/", row);
+        // singleFile << result << ",";
+        allFile << "SIS," << i << "," << result << std::endl;
+
+        result = ModelRunner::run_myAPMISRR(
+            30, 0.5, m[index_w], w, 0.3, "../data/exp1-30-servers/", row,
+            error_installment[index_w][index_30]);
+        // singleFile << result << "\n";
+        allFile << "APMISRR," << i << "," << result << std::endl;  
+        
+        result =
+            ModelRunner::run_MISRR(30, 0, w, 0.3, "../data/exp1-30-servers/",
+                                   row, error_installment[index_w][index_30]);
+        // singleFile << result << ",";
+        allFile << "TolerMIS," << i << "," << result << std::endl; 
+        
         index_30++;
       }
       index_w++;
     }
-    singleFile.close();
+    // singleFile.close();
   }
 }
 
@@ -334,35 +351,44 @@ void error_save_each_time() {
     readTXTFile("../data/exp3-error-place/error-installment-2.txt",
                 error_installment);
 
+    std::ofstream allFile("../output/exp_3_all_exps/error_time_s30_n" +
+                          std::to_string(i) + ".csv");
+    allFile << "model,workload,value" << std::endl;
+
     /* 任务量w */
     int index_w = 0;
     for (int w = 5000; w < 16000; w += 1000) {
       int index_30 = 0;
       double result_sum = 0;
       double result_min = 100000, result_max = 0;
-      std::ofstream singleFile("../output/exp_3_all_exps/error_time_s30_n" +
-                               std::to_string(i) + "_W" + std::to_string(w) +
-                               ".csv");
-      singleFile << "SIS,APMISRR,TolerMIS\n";
+      // std::ofstream singleFile("../output/exp_3_all_exps/error_time_s30_n" +
+      //                          std::to_string(i) + "_W" + std::to_string(w) +
+      //                          ".csv");
+      // singleFile << "SIS,APMISRR,TolerMIS\n";
 
       /* 每种30次取平均 */
       for (const auto &row : error_places) {
-        double result = ModelRunner::run_SIS(30, w, 0.3, "../data/exp1-30-servers/", row);
-        singleFile << result << ",";
-        result = ModelRunner::run_myAPMISRR(30, 0.5, m[index_w], w, 0.3,
-                               "../data/exp1-30-servers/", row,
-                               error_installment[index_w][index_30]);
-        singleFile << result << ",";
-        result = ModelRunner::run_MISRR(30, 0, w, 0.3, "../data/exp1-30-servers/", row,
-                           error_installment[index_w][index_30++]);
-        singleFile << result << "\n";
+        double result =
+            ModelRunner::run_SIS(30, w, 0.3, "../data/exp1-30-servers/", row);
+        allFile << "SIS," << w << "," << result << std::endl;
+        // singleFile << result << ",";
+        result = ModelRunner::run_myAPMISRR(
+            30, 0.5, m[index_w], w, 0.3, "../data/exp1-30-servers/", row,
+            error_installment[index_w][index_30]);
+        allFile << "APMISRR," << w << "," << result << std::endl;
+        // singleFile << result << ",";
+        result =
+            ModelRunner::run_MISRR(30, 0, w, 0.3, "../data/exp1-30-servers/",
+                                   row, error_installment[index_w][index_30++]);
+        allFile << "TolerMIS," << w << "," << result << std::endl;
+        // singleFile << result << "\n";
         result_sum += result;
         result_min = std::min(result_min, result);
         result_max = std::max(result_max, result);
       }
 
       index_w++;
-      singleFile.close();
+      // singleFile.close();
     }
   }
 }
