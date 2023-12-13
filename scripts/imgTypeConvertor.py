e8614ed52a38f78ccb97f246b8c25ef80d73565c
rthete <603409022@qq.com>
2023-12-13 16:07:44 +0800

    fix: 作图相关

--- scripts/imgTypeConvertor.py
+++ scripts/imgTypeConvertor.py
@@ -3,10 +3,11 @@ FilePath: \InstallmentScheduling\scripts\imgTypeConvertor.py
 Description:  
 Author: rthete
 Date: 2023-09-01 00:47:08
-LastEditTime: 2023-09-09 13:09:37
+LastEditTime: 2023-12-03 22:03:51
 '''
+import os
 from PIL import Image
-import cairosvg
+# import cairosvg
 
 # png --> eps 
 def png2eps(input_path, output_path):
@@ -23,10 +24,28 @@ def png2eps(input_path, output_path):
         print("转换失败：", str(e))
         
 # svg --> pdf
-def svg2pdf(input_svg, output_pdf):
-    cairosvg.svg2pdf(file_obj=open(input_svg, 'rb'), write_to=output_pdf)
+# def svg2pdf(input_svg, output_pdf):
+#     cairosvg.svg2pdf(file_obj=open(input_svg, 'rb'), write_to=output_pdf)
+
+def process_images_in_folder(input_path):
+    # 获取文件夹中所有文件
+    files = os.listdir(input_path)
+
+    # 遍历文件夹中的每个文件
+    for file in files:
+        # 拼接文件的完整路径
+        file_path = os.path.join(input_path, file)
+
+        # 检查文件是否是PNG图片
+        if file.lower().endswith(".png"):
+            # 调用processor函数处理图片
+            output_path = os.path.join("D:\\OneDrive - stu.xidian.edu.cn\\workspace\\任务调度相关\\实验\\TolerMIS实验\\231129_eps格式图例\\eps\\故障处理实验\\using_rate", 
+                                       os.path.splitext(os.path.basename(file_path))[0] + ".eps")
+            png2eps(file_path, output_path)
 
 if __name__ == "__main__":
-    input_path = "T vs. theta (n=30).svg"
+    # input_path = "T vs. theta (n=30).svg"
     # png2eps(input_path, "output.eps")
-    svg2pdf(input_path, "output.pdf")
+    # svg2pdf(input_path, "output.pdf")
+    folder_path = "D:\\OneDrive - stu.xidian.edu.cn\\workspace\\任务调度相关\\实验\\TolerMIS实验\\231129_eps格式图例\\png\\故障处理实验\\using_rate"
+    process_images_in_folder(folder_path)
